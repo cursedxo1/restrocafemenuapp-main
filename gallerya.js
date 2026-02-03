2c83b02dd145d67d711cf68ac024cac98e82f57e
chitransh <148608225+cursedxo1@users.noreply.github.com>
2026-02-03 20:16:28 +0530

    Add files via upload

---
+++ gallerya.js
@@ -0,0 +1,4 @@
+var app = new Vue({ 
+    el: '#app',
+});
+document.write( '<center><p><small><small>made with ❤ by <a href=\'https://a5-h-development-studio.vercel.app/\' class=\'text-danger\' target=\'blank\'>https://a5-h-development-studio.vercel.app/</a> <br/></small></small></p></center>' );
